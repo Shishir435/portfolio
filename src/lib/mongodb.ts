cbf821fe7a8ba2c0ed4ae4719b86241d36ea4c87
Shishir435 <Shishirchaurasiya435@gmail.com>
2023-11-04 21:53:19 +0530

    connections

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,55 +1,10 @@
 
 
 
-import mongoose, { ConnectOptions, Connection } from 'mongoose';
-
-let isConnected = false;
-let db: Connection | null = null;
-
-export async function connect() {
-  try {
-    if (isConnected) {
-      console.log('MongoDB is already connected');
-      return;
-    }
-
-    db = await mongoose.createConnection(process.env.NEXT_PUBLIC_MONGODB_URL!, {
-    //   useUnifiedTopology: true,
-    } as ConnectOptions);
-
-    db.on('connected', () => {
-      console.log('MongoDB connected successfully');
-      isConnected = true;
-    });
-
-    db.on('error', (err) => {
-      console.error('MongoDB connection error:', err);
-      process.exit(1); // Exit the application with an error code
-    });
-
-    db.on('disconnected', () => {
-      console.log('MongoDB disconnected');
-      isConnected = false;
-    });
-  } catch (error) {
-    console.error('Error while connecting to MongoDB:', error);
-    isConnected = false;
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-// import mongoose from 'mongoose';
+// import mongoose, { ConnectOptions, Connection } from 'mongoose';
 
 // let isConnected = false;
+// let db: Connection | null = null;
 
 // export async function connect() {
 //   try {
@@ -58,27 +13,72 @@ export async function connect() {
 //       return;
 //     }
 
-//     await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URL!, {
-//       // Other options can be added here if needed
-//     });
+//     db = await mongoose.createConnection(process.env.NEXT_PUBLIC_MONGODB_URL!, {
+//     //   useUnifiedTopology: true,
+//     } as ConnectOptions);
 
-//     const connection = mongoose.connection;
-
-//     connection.once('open', () => {
+//     db.on('connected', () => {
 //       console.log('MongoDB connected successfully');
 //       isConnected = true;
 //     });
 
-//     connection.on('error', (err) => {
-//       console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
+//     db.on('error', (err) => {
+//       console.error('MongoDB connection error:', err);
 //       process.exit(1); // Exit the application with an error code
 //     });
+
+//     db.on('disconnected', () => {
+//       console.log('MongoDB disconnected');
+//       isConnected = false;
+//     });
 //   } catch (error) {
-//     console.log('Something went wrong while connecting to MongoDB');
-//     console.error(error);
+//     console.error('Error while connecting to MongoDB:', error);
 //     isConnected = false;
 //   }
 // }
+
+
+
+
+
+
+
+
+
+
+
+import mongoose from 'mongoose';
+
+let isConnected = false;
+
+export async function connect() {
+  try {
+    if (isConnected) {
+      console.log('MongoDB is already connected');
+      return;
+    }
+
+    await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URL!, {
+      // Other options can be added here if needed
+    });
+
+    const connection = mongoose.connection;
+
+    connection.once('open', () => {
+      console.log('MongoDB connected successfully');
+      isConnected = true;
+    });
+
+    connection.on('error', (err) => {
+      console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
+      process.exit(1); // Exit the application with an error code
+    });
+  } catch (error) {
+    console.log('Something went wrong while connecting to MongoDB');
+    console.error(error);
+    isConnected = false;
+  }
+}
 
 
 // import mongoose from 'mongoose';
