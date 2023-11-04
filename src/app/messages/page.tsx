50f087339c9cbed0631ea3053ffbda1b0d7ec5d4
Shishir435 <Shishirchaurasiya435@gmail.com>
2023-11-04 06:01:59 +0530

    auth err

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import AuthAdminCheck from "@/components/AuthAdminCheck";
 import { Button } from "@/components/ui/button";
-import axios from "axios";
-import { ChangeEvent, useEffect, useState } from "react";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
   Table,
   TableBody,
@@ -12,37 +12,32 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Checkbox } from "@/components/ui/checkbox"
-import AuthAdminCheck from "@/components/AuthAdminCheck";
+import axios from "axios";
+import { useState } from "react";
 
 interface ContactMessage {
   _id: string;
   name: string;
   email: string;
   message: string;
-  resolve: boolean
+  resolve: boolean;
 }
 
-
 const page = () => {
-  const [ContactMessages, setContactMessages] = useState<ContactMessage[]>([
-    
-  ]);
+  const [ContactMessages, setContactMessages] = useState<ContactMessage[]>([]);
 
- 
   const [loading, setLoading] = useState(false);
-  const [isResolveing,setIsResolving]=useState(false)
-   const [IsAdmin, setIsAdmin] = useState(false)
+  const [isResolveing, setIsResolving] = useState(false);
+  const [IsAdmin, setIsAdmin] = useState(false);
 
-  function stringTobool(data:string){
-
-    if(data==="true") return true
-     return false
+  function stringTobool(data: string) {
+    if (data === "true") return true;
+    return false;
   }
   async function handleCheckBoxChnage(_id: string, value: boolean | string) {
     // Convert the value to a boolean if it's a string
     const boolVal = typeof value === "string" ? stringTobool(value) : value;
-  
+
     setContactMessages((prevContactMessages) => {
       return prevContactMessages.map((message) => {
         if (message._id === _id) {
@@ -52,20 +47,20 @@ const page = () => {
         return message;
       });
     });
-  
+
     const patchData = {
       id: _id,
       resolveValue: boolVal,
     };
-  
+
     try {
       setIsResolving(true);
-      const response = await axios.patch('/api/portfolio/contact', patchData);
+      const response = await axios.patch("/api/portfolio/contact", patchData);
       // Successful API call, no need for additional UI updates
       // console.log(response);
     } catch (error) {
       console.error("handleCheckBoxChnage", error);
-  
+
       // If the API call fails, revert the local state to its previous value
       setContactMessages((prevContactMessages) => {
         return prevContactMessages.map((message) => {
@@ -79,7 +74,7 @@ const page = () => {
       setIsResolving(false);
     }
   }
-  
+
   async function handleClick() {
     try {
       setLoading(true);
@@ -94,7 +89,7 @@ const page = () => {
     }
   }
 
-  return IsAdmin?(
+  return IsAdmin ? (
     <div className="flex flex-col justify-center items-center p-6 gap-10">
       <Button onClick={handleClick}>
         {loading ? "fetching messages" : "fetch messages"}
@@ -111,18 +106,18 @@ const page = () => {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {ContactMessages.map(({ _id, name, email, message,resolve }) => (
+            {ContactMessages.map(({ _id, name, email, message, resolve }) => (
               <TableRow key={_id}>
                 <TableCell className="font-medium">{name}</TableCell>
                 <TableCell>{email}</TableCell>
                 <TableCell>{message}</TableCell>
                 <TableCell>
                   <Checkbox
-                  checked={resolve}
-                  onCheckedChange={(value)=>{
-                    //  console.log(value)
-                    handleCheckBoxChnage(_id,value)
-                  }}
+                    checked={resolve}
+                    onCheckedChange={(value) => {
+                      //  console.log(value)
+                      handleCheckBoxChnage(_id, value);
+                    }}
                   />
                 </TableCell>
               </TableRow>
@@ -131,7 +126,9 @@ const page = () => {
         </Table>
       </div>
     </div>
-  ):(<AuthAdminCheck setIsAdmin={setIsAdmin}/>);
+  ) : (
+    <AuthAdminCheck setIsAdmin={setIsAdmin} />
+  );
 };
 
 export default page;
