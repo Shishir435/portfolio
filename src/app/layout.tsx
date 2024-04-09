4f3d88063a6115be7d3dac8a61787bc96485b04e
Shishir435 <shishirchaurasiya435@gmail.com>
2024-04-09 23:22:23 +0530

    Add @vercel/speed-insights package and import SpeedInsights component in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 import { Toaster } from "@/components/ui/sonner"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export const metadata: Metadata = {
   title: "Shishir's Portfolio",
   description: "Full-Stack web Developer",
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={`${inter.className}`}>
         {children}
         <Analytics />
+        <SpeedInsights />
         <Toaster richColors closeButton />
       </body>
     </html>
