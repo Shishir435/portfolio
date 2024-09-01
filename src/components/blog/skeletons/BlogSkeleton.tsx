da7b362dfe5fe5a56639cad8127ba72e9bf4e3fb
shishir435 <shishirchaurasiya435@gmail.com>
2024-09-01 14:40:25 +0530

    refactor: Update skeleton components for blog page

--- src/components/blog/skeletons/BlogSkeleton.tsx
+++ src/components/blog/skeletons/BlogSkeleton.tsx
@@ -3,14 +3,13 @@ import React from "react"
 
 const BlogSkeleton = () => {
   return (
-    <div className="min-h-dvh">
-      <Skeleton className="h-24 w-full rounded-sm bg-slate-400/30" />
-      <Skeleton className="h-24 w-full rounded-sm bg-slate-400/30" />
-      <Skeleton className="h-24 w-full rounded-sm bg-slate-400/30" />
-      <Skeleton className="h-24 w-full rounded-sm bg-slate-400/30" />
-      <Skeleton className="h-24 w-full rounded-sm bg-slate-400/30" />
-      <Skeleton className="h-24 w-full rounded-sm bg-slate-400/30" />
-      <Skeleton className="h-24 w-full rounded-sm bg-slate-400/30" />
+    <div className="mt-6 flex flex-col gap-y-6">
+      {Array.from({ length: 6 }).map((_, index) => (
+        <Skeleton
+          key={index}
+          className="h-20 w-[28rem] bg-slate-200 dark:bg-slate-600 sm:w-[38rem] md:w-[47rem] lg:w-[70rem]"
+        />
+      ))}
     </div>
   )
 }
