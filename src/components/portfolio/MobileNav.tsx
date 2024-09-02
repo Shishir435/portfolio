4565d1d55f12d5eb77bea609eb68ff823ae8ba14
shishir435 <shishirchaurasiya435@gmail.com>
2024-09-02 08:26:33 +0530

    refactor: Update MobileNav component to use DropdownMenu for improved mobile navigation experience

--- src/components/portfolio/MobileNav.tsx
+++ src/components/portfolio/MobileNav.tsx
@@ -1,50 +1,42 @@
 "use client"
-
-import { Menu } from "lucide-react"
-import { useRef, useState } from "react"
-import { buttonVariants } from "../ui/button"
-import Link from "next/link"
 import { MobileNavLinks } from "@/lib/content"
-import { useClickAway } from "react-use"
+import { Menu } from "lucide-react"
+import Link from "next/link"
+import { Button, buttonVariants } from "../ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu"
 
 const MobileNav = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-  const divRef = useRef<HTMLDivElement>(null)
-  useClickAway(divRef, () => setIsMenuOpen(false))
   return (
-    <>
-      <div
-        className="cursor-pointer md:hidden"
-        onClick={() => {
-          setIsMenuOpen((prev) => !prev)
-        }}
-      >
-        <Menu />
-      </div>
-      {isMenuOpen && (
-        <div
-          ref={divRef}
-          className="fixed right-[80px] top-[80px] z-40 w-32 rounded-md bg-slate-200 p-4"
-        >
-          <div className="flex w-full flex-col text-center">
-            {MobileNavLinks.map(
-              ({ linkName, linkTo }: { linkName: string; linkTo: string }) => (
-                <div onClick={() => setIsMenuOpen(false)} key={linkName}>
-                  <Link
-                    href={linkTo}
-                    className={`${buttonVariants({
-                      variant: "ghost",
-                    })} w-full capitalize text-black `}
-                  >
-                    {linkName}
-                  </Link>
-                </div>
-              )
-            )}
-          </div>
-        </div>
-      )}
-    </>
+    <div className="cursor-pointer md:hidden">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="link" size="icon">
+            <Menu />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          {MobileNavLinks.map(
+            ({ linkName, linkTo }: { linkName: string; linkTo: string }) => (
+              <DropdownMenuItem key={linkName}>
+                <Link
+                  href={linkTo}
+                  className={`${buttonVariants({
+                    variant: "ghost",
+                  })} w-full capitalize text-black `}
+                >
+                  {linkName}
+                </Link>
+              </DropdownMenuItem>
+            )
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   )
 }
 
