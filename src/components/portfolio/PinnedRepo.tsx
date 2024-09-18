1ca4c06a7fa263ead03ba66a858feccdbfa26bd4
shishir435 <shishirchaurasiya435@gmail.com>
2024-09-18 05:49:11 +0530

    refactor: Update project title and remove unnecessary paragraph

--- src/components/portfolio/PinnedRepo.tsx
+++ src/components/portfolio/PinnedRepo.tsx
@@ -21,16 +21,8 @@ export default function PinnedRepo() {
   return (
     <section id="work" className="mt-24">
       <div className="mx-auto max-w-6xl p-6">
-        <p>Portfolio</p>
-        <h2 className="my-4 text-4xl font-bold">My recent Works</h2>
-        <div className="flex w-full">
-          <p className="mt-3 max-w-3xl text-[17px] leading-[30px]">
-            The projects listed below demonstrate my abilities and skills
-            through real-world examples of my work. Each project has a brief
-            description and links to code repositories and live demos.
-          </p>
-        </div>
-        <div className="xs:grid-cols-1 mt-20 grid grid-cols-1  gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+        <h2 className="my-4 text-3xl font-bold">Personal Projects</h2>
+        <div className="xs:grid-cols-1 mt-10 grid grid-cols-1  gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
           {isLoading && (
             <>
               {[...Array(3)].map((_, index) => (
